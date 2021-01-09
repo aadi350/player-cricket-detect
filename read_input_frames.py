0ec22a3a63d7a5e5034bf9f62c35effda7462c3d
aadi350 <a.aadi380@gmail.com>
2021-01-09 19:57:10 -0400

    remove img_to_array call

--- read_input_frames.py
+++ read_input_frames.py
@@ -2,21 +2,28 @@ import logging
 import os
 import numpy as np
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-
+import matplotlib.pyplot as plt
 from util import numerical_sort
 
-TARGET_SIZE = (244, 244, 3)
+TARGET_SIZE = (720, 1280, 3)
+CAT_PATH = 'D:/_assets/img/sahil_frames'
 
 
-def load_single_video_frames(path):
+def load_video_frames(path=CAT_PATH, num_frames=None, output_names=False):
     frame_list = []
+    frame_dict = {}
     try:
-        for file in sorted(os.listdir(path), key=numerical_sort):
+        for i, file in enumerate(sorted(os.listdir(path), key=numerical_sort)):
             if file.endswith('.jpg'):
-                print(os.path.join(path, file))
-                frame = load_img(os.path.join(path, file), target_size=TARGET_SIZE, color_mode='rgb')
-                frame_temp = img_to_array(frame)
-                frame_list.append(frame_temp)
-        return np.array(frame_list)
+                logging.debug(os.path.join(path, file))
+                frame = load_img(os.path.join(path, file), target_size=TARGET_SIZE)
+                frame_list.append(frame)
+                frame_dict[str(file)] = frame
+            if num_frames is not None:
+                if i >= num_frames - 1:
+                    return frame_list, frame_dict.keys()
+        return np.array(frame_list), frame_dict.keys()
     except ImportError:
         logging.error('Video read fail')
+
+

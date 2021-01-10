d360ae57adbde585254ca9ad0d88083030650864
aadi350 <a.aadi380@gmail.com>
2021-01-10 09:04:45 -0400

    return file names

--- read_input_frames.py
+++ read_input_frames.py
@@ -1,29 +1,42 @@
 import logging
 import os
+from six import BytesIO
+import PIL
 import numpy as np
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import matplotlib.pyplot as plt
+import tensorflow as tf
+from PIL import Image
 from util import numerical_sort
-
-TARGET_SIZE = (720, 1280, 3)
 CAT_PATH = 'D:/_assets/img/sahil_frames'
 
 
-def load_video_frames(path=CAT_PATH, num_frames=None, output_names=False):
+def _process_path(fname):
+    img_str = tf.io.read_file(fname)
+    img = tf.image.decode_jpeg(img_str, channels=3)
+    return img
+
+
+def load_video_frames(path=CAT_PATH, num_frames=None):
     frame_list = []
+    frame_names = []
     frame_dict = {}
+
     try:
         for i, file in enumerate(sorted(os.listdir(path), key=numerical_sort)):
             if file.endswith('.jpg'):
                 logging.debug(os.path.join(path, file))
-                frame = load_img(os.path.join(path, file), target_size=TARGET_SIZE)
+                
+                full_path = os.path.join(path, file)
+                frame = np.asarray(Image.open(full_path))
+                print(type(frame))
                 frame_list.append(frame)
+                frame_names.append(str(file))
                 frame_dict[str(file)] = frame
             if num_frames is not None:
                 if i >= num_frames - 1:
-                    return frame_list, frame_dict.keys()
-        return np.array(frame_list), frame_dict.keys()
+                    return frame_list, frame_names
+        return np.asarray(frame_list, dtype=np.uint8), frame_dict.keys()
     except ImportError:
         logging.error('Video read fail')
 
 
+load_video_frames(num_frames=5)

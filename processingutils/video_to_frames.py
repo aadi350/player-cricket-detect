20da0f3d003e67c06209b95c95dc9d11841291df
aadi350 <a.aadi380@gmail.com>
2021-01-17 09:03:04 -0400

    refactor frame extraction for folder creation

--- processingutils/video_to_frames.py
+++ processingutils/video_to_frames.py
@@ -12,6 +12,10 @@ logger.debug(tf.__version__)
 numbers = re.compile(r'(\d+)')
 tf.compat.v1.disable_eager_execution()
 
+VIDEO_PATH = '/home/aadi/PycharmProjects/player-cricket-detect/data/video/sahil_videos'
+TARGET_PATH = '/home/aadi/PycharmProjects/player-cricket-detect/data/img/frames'
+
+
 # utility sort function
 def numerical_sort(value):
     parts = numbers.split(value)
@@ -19,42 +23,45 @@ def numerical_sort(value):
     return parts
 
 
-def extract_single_video(path):
-    vid = cv.VideoCapture(path)
-    frames_path = path.split('.')[0]
-    logger.debug(frames_path)
-    if not os.path.isdir(frames_path):
-        os.mkdir(frames_path)
-        logging.debug('path not exist')
-    logger.debug('frames_path: {}'.format(frames_path))
-    frame_num = 0
-    while True:
-        success, frame = vid.read()
-        if not success:
-            logger.debug('fail: {}'.format(frame_num))
-            break
-        temp_name = os.path.join(frames_path, os.path.basename(path).split('.')[0] + '_f' + str(frame_num) + '.jpg')
-        cv.imwrite(temp_name, frame)
-        frame_num += 1
-    vid.release()
-    return
-
-
-def get_file_path():
-    paths = {
-        "Windows": "D:/_assets/video/sahil_videos",
-        "Linux": "/media/aadi/Library1/_assets/video/sahil_videos"
-    }
-    import platform
-    return paths.get(platform.system())
+def extract_single_video(videopath, targetpath=TARGET_PATH):
+    vid = cv.VideoCapture(videopath)
+    video_name = (videopath.split('.')[0]).split('/')[-1]
+    video_frames_path = os.path.join(targetpath, video_name)
+    logger.debug('video_name: {}, video_frames_path: {}'.format(video_name, video_frames_path))
+    if not os.path.isdir(video_frames_path):
+        try:
+            os.mkdir(video_frames_path)
+            logging.debug('path not exist, path created')
+            frame_num = 0
+            while True:
+                success, frame = vid.read()
+                if not success:
+                    logger.debug('fail: {}'.format(frame_num))
+                    break
+                temp_path = os.path.join(video_frames_path,
+                                         os.path.basename(videopath).split('.')[0] + '_f' + str(frame_num) + '.jpg')
+                logger.debug('temp_path: {}'.format(temp_path))
+                cv.imwrite(temp_path, frame)
+                frame_num += 1
+            vid.release()
+            return
+        except Exception:
+            logging.error('Could not create folder for: '.format(video_name))
+            pass
+        return
 
 
 def main():
-    video_path = get_file_path()
-    for file in sorted(os.listdir(video_path), key=numerical_sort):
+    if not os.path.isdir(TARGET_PATH):
+        try:
+            logging.debug('path not exist')
+            os.mkdir(TARGET_PATH)
+        except FileExistsError('cannot create frames folder'):
+            pass
+
+    for file in sorted(os.listdir(VIDEO_PATH), key=numerical_sort):
         if file.endswith('.mp4'):
-            extract_single_video(os.path.join(video_path, file))
-        break
+            extract_single_video(os.path.join(VIDEO_PATH, file))
 
 
 if __name__ == '__main__':

5d89ac699b0f8833ee4c4158caf81c6fc24e08e6
aadi350 <a.aadi380@gmail.com>
2021-01-18 06:18:28 -0400

    Edit to use native Keras callbacks

--- models/mobilenet/tune.py
+++ models/mobilenet/tune.py
@@ -11,7 +11,7 @@ import models.mobilenet.mobilenet as mobilenet
 
 EPOCHS = 1
 MODEL = 'mobilenet'
-LOGDIR = "logs/hparam_tuning/" + MODEL + '_' + datetime.now().strftime("%Y%m%d-%H%M%S") + '_'
+LOGDIR = "logs/hparam_tuning/mobilenet"
 
 (train_gen, val_gen), class_dict = dataset_generator.get_datagen()
 # Specify discrete OR ranges of hyper-parameter intervals
@@ -20,12 +20,6 @@ HP_DROPOUT = hp.HParam('dropout', hp.RealInterval(0.1, 0.3))
 HP_OPTIMIZER = hp.HParam('optimizer', hp.Discrete(['adam', 'sgd']))
 
 METRIC_ACCURACY = 'accuracy'
-# set up logging with parameters
-with tf.summary.create_file_writer(LOGDIR).as_default():
-    hp.hparams_config(
-        hparams=[HP_NUM_UNITS, HP_DROPOUT, HP_OPTIMIZER],
-        metrics=[hp.Metric(METRIC_ACCURACY, display_name='Accuracy')],
-    )
 
 
 # Harness for setting up model for each configuration
@@ -39,18 +33,22 @@ def train_test_model(hparams, train_data, val_data):
 
     model.fit(
         train_data,
-        epochs=EPOCHS
+        epochs=EPOCHS,
+        callbacks=[
+            tf.keras.callbacks.TensorBoard(LOGDIR),  # log metrics
+            hp.KerasCallback(LOGDIR, hparams),
+        ]
     )
     _, accuracy = model.evaluate(val_data)
     return accuracy
 
 
 # Harness for running model with logs
-def run(run_dir, hparams, train_data, val_data):
-    with tf.summary.create_file_writer(run_dir).as_default():
-        hp.hparams(hparams)  # record the values used in this trial
-        accuracy = train_test_model(hparams, train_data, val_data)
-        tf.summary.scalar(METRIC_ACCURACY, accuracy, step=1)
+def run(hparams, train_data, val_data):
+    hp.hparams(hparams)  # record the values used in this trial
+    accuracy = train_test_model(hparams, train_data, val_data)
+    tf.summary.scalar(METRIC_ACCURACY, accuracy, step=1)
+
 
 # Grid search using every combination of discrete values
 # TODO find a more efficient way of doing this
@@ -65,8 +63,6 @@ for num_units in HP_NUM_UNITS.domain.values:
             }
             run_name = "run-%d" % session_num
             print('--- Starting trial: %s' % run_name)
-            run('logs/hparam_tuning/' + run_name, hparams, train_gen, val_gen)
+            run(hparams, train_gen, val_gen)
             print({h: hparams[h] for h in hparams})
             session_num += 1
-
-
